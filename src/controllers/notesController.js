c862a3e4a210d0556ccd213fc398130c47e639dc
Oleksii Mushtaiev <thascarfaze@gmail.com>
2025-11-09 11:12:30 +0400

    Update notesController.js

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,7 +2,7 @@ import { Note } from '../models/note.js';
 import createHttpError from 'http-errors';
 
 // Отримати список усіх нотаток
-export const getNotes = async (req, res) => {
+export const getAllNotes = async (req, res) => {
   const notes = await Note.find();
   res.status(200).json(notes);
 };
